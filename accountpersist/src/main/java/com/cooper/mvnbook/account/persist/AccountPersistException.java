9ca811927de1ae2ca4e2102921b4c59581dff916
sunzhilong <szllong@cqu.edu.cn>
2019-04-08 01:32:18 +0800

    添加类

---
+++ accountpersist/src/main/java/com/cooper/mvnbook/account/persist/AccountPersistException.java
@@ -0,0 +1,11 @@
+package com.cooper.mvnbook.account.persist;
+
+public class AccountPersistException extends Exception {
+	public AccountPersistException(String message) {
+		super(message);
+	}
+
+	public AccountPersistException(String message, Throwable throwable) {
+		super(message, throwable);
+	}
+}
